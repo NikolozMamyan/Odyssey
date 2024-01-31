5c7c274363241a8490a67f79f429626934a246fd
leoKowalski <leo.kowalski4872@gmail.com>
2024-01-31 17:29:05 +0100

    commented env DB line; created Teacher and Course basic model

---
+++ src/Entity/Course.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CourseRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CourseRepository::class)]
+class Course
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $courseTitle = null;
+
+    #[ORM\Column(length: 600, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\ManyToOne(inversedBy: 'createdCourses')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Teacher $author = null;
+
+    #[ORM\Column(length: 10000)]
+    private ?string $content = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCourseTitle(): ?string
+    {
+        return $this->courseTitle;
+    }
+
+    public function setCourseTitle(string $courseTitle): static
+    {
+        $this->courseTitle = $courseTitle;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?Teacher
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(?Teacher $author): static
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+}
