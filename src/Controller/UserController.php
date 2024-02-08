6e2c58bb061054fc338dbab9850a8dedc0e14821
NikolozMamyan <nikolozmamyan@gmail.com>
2024-02-08 09:43:41 +0100

    cardRename

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,15 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UserController extends AbstractController
 {
     #[Route('/user', name: 'app_user')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager, UserRepository $userRepository, Request $request, int $id): Response
     {
+        $user= $userRepository->find($id);
+        
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
         ]);
