5f2d8dc0d77aa41e750ce0bf195ed5bf8043da1d
Stephane Koeniguer <stephane.koeniguer@gmail.com>
2024-02-10 17:17:51 +0100

    feature mail

--- src/Controller/LandingPageController.php
+++ src/Controller/LandingPageController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Service\MailerService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class LandingPageController extends AbstractController
 {
@@ -15,4 +18,26 @@ class LandingPageController extends AbstractController
             'controller_name' => 'LandingPageController',
         ]);
     }
+
+    #[Route('/contact', name: 'app_contact', methods:['POST'])]
+    public function contact(MailerService $mailerService, Request $request, ValidatorInterface $validator): Response
+    {
+        // recover data from form
+        $data['name'] = $request->request->get('name');
+        $data['email'] = $request->request->get('email');
+        $data['message'] = $request->get('message');
+
+
+        // Call the mailer service
+        $message = $mailerService->sendMail($data);
+
+        if ($message === 'Votre message a bien été envoyé' ) {
+            $this->addFlash('success', $message);
+        } else {
+            $this->addFlash('danger', $message);
+        }
+
+        return $this->redirectToRoute('app_landing_page');
+    }
+
 }
