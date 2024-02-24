c68648d2350e07114f660323dbaca23745512e13
Guoying <86628141+Guoying-Li@users.noreply.github.com>
2024-02-24 17:19:52 +0100

    vich bundle

--- src/Entity/Course.php
+++ src/Entity/Course.php
@@ -7,8 +7,11 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints\Cascade;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 #[ORM\Entity(repositoryClass: CourseRepository::class)]
+#[Vich\Uploadable]
 class Course
 {
     #[ORM\Id]
@@ -18,6 +21,19 @@ class Course
 
     #[ORM\Column(length: 255)]
     private ?string $title = null;
+
+    
+    #[ORM\Column(type:"datetime_immutable", nullable:true)]
+    private $updatedAt;
+
+
+       
+    #[Vich\UploadableField(mapping: 'courses', fileNameProperty: 'imageName')]
+    private ?File $imageFile = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?string $imageName = null;
+
 
     #[ORM\Column(length: 600, nullable: true)]
     private ?string $description = null;
@@ -60,6 +76,41 @@ class Course
         $this->title = $title;
 
         return $this;
+    }
+
+        /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
+     */
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function setImageName(?string $imageName): void
+    {
+        $this->imageName = $imageName;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
     }
 
     public function getDescription(): ?string
--- src/Form/CourseType.php
+++ src/Form/CourseType.php
@@ -12,6 +12,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class CourseType extends AbstractType
 {
@@ -28,6 +29,13 @@ class CourseType extends AbstractType
     {
 
         $builder
+            ->add('imageFile', VichImageType::class, [
+                'label' => 'Image du course',
+                'label_attr'=> [
+                    'class' => 'form-label mt-4'
+                ],
+                'required'=> true, 
+            ])
             ->add('title', TextType::class)
             ->add('categories', EntityType::class, [
                 'class' => Category::class,
