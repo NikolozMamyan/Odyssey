916fcbf03a93d1fdb4c683f841d1906102086475
NikolozMamyan <nikolozmamyan@gmail.com>
2024-03-08 11:33:22 +0100

    phpunitTest_Register_NikolozM

--- tests/Controller/UserControllerTest.php
+++ tests/Controller/UserControllerTest.php
@@ -15,15 +15,15 @@ class UserControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/login');
 
         $form = $crawler->selectButton('Valider')->form([
-            'username' => 'Fabien@symfony.com',
-            'password' => 'azertyuioP1',
+            'username' => 'test@test.com',
+            'password' => 'azertyP1',
         ]);
 
         $client->submit($form);
 
 
         // Crée une requête HTTP GET pour accéder à la page d'édition d'utilisateur
-        $client->request('GET', '/user/edit/1');
+        $client->request('GET', '/user/edit/5');
 
         // Vérifie que la réponse est un succès après la redirection
         $this->assertResponseIsSuccessful('La redirection vers la page de modification de profil à fonctionné.');
@@ -40,7 +40,7 @@ class UserControllerTest extends WebTestCase
         $this->assertResponseIsSuccessful();
 
         // permet de vérifier que la modification est présente sur la page
-        $this->assertSelectorExists('p:contains("Potencier")');
+         $this->assertSelectorExists('p:contains("Potencier")');
 
     }
 }
